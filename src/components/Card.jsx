62705e50d8e1379d15023095a26fad48bae16a82
Alexandre Medina <alesurf13@gmail.com>
2023-01-29 14:15:08 -0300

    ajuste card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,23 @@
 import { AiFillEye } from "react-icons/ai";
 
-export function Card() {
+export function Card({ item }) {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6">
+    <div className="bg-white rounded-lg shadow-sm p-6">
       <div className="flex items-center">
         <div className="flex-shrink-0">
-          <img
-            className="h-12 w-12 rounded-full"
-            src="https://i.pravatar.cc/300"
-            alt=""
-          />
+          <img className="h-12 w-12 rounded-full" src={item.avatar} alt="" />
         </div>
         <div className="ml-4">
-          <div className="text-sm font-medium text-gray-900">Juse</div>
-          <div className="text-sm text-gray-500">Teste</div>
+          <div className="text-sm font-medium text-gray-900">{item.name}</div>
+          <div className="text-sm text-gray-500">{item.description}</div>
         </div>
         <div className="ml-auto flex items-center justify-center px-2 py-1 text-xs font-medium rounded-full text-green-800 bg-green-100">
           <AiFillEye size={20} className="ml-4 text-gray-500" />
-          <span className="ml-1 text-sm text-gray-500">100 views</span>
+          <span className="ml-1 text-sm text-gray-500">{item.views} views</span>
         </div>
       </div>
       <div className="mt-6">
-        <img
-          className="w-full "
-          src="https://static.valorantzone.gg/news/2020/04/27150238/BIND.png"
-          alt=""
-        />
+        <img className="w-full " src={item.image} alt="" />
       </div>
     </div>
   );
--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,48 +1,257 @@
 import { Card } from "./Card";
 import { NamePage } from "./NamePage";
 
+const cardInfos = [
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Juse",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Domo",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Evandro",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Nandinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 5,
+    avatar: "https://i.pravatar.cc/300",
+    name: "leozinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 6,
+    avatar: "https://i.pravatar.cc/300",
+    name: "alesurf13",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Juse",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Domo",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Evandro",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Nandinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 5,
+    avatar: "https://i.pravatar.cc/300",
+    name: "leozinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 6,
+    avatar: "https://i.pravatar.cc/300",
+    name: "alesurf13",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Juse",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Domo",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Evandro",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Nandinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 5,
+    avatar: "https://i.pravatar.cc/300",
+    name: "leozinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 6,
+    avatar: "https://i.pravatar.cc/300",
+    name: "alesurf13",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Juse",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Domo",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Evandro",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Nandinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 5,
+    avatar: "https://i.pravatar.cc/300",
+    name: "leozinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 6,
+    avatar: "https://i.pravatar.cc/300",
+    name: "alesurf13",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Juse",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Domo",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Evandro",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/300",
+    name: "Nandinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 5,
+    avatar: "https://i.pravatar.cc/300",
+    name: "leozinhe",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+  {
+    id: 6,
+    avatar: "https://i.pravatar.cc/300",
+    name: "alesurf13",
+    description: "Teste",
+    views: 100,
+    image: "https://static.valorantzone.gg/news/2020/04/27150238/BIND.png",
+  },
+];
+
 export function ContainerCard() {
+  const cardMap = cardInfos.map((item, key) => <Card item={item} key={key} />);
+
   return (
     <section className="flex-1">
       <NamePage label="All Pixels" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-6">
+        {cardMap}
       </div>
     </section>
   );
