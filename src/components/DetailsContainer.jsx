309e7827eba96e351ac85aac72ad7ef8f854bed8
Alexandre Medina <alesurf13@gmail.com>
2023-01-29 15:59:31 -0300

    create pag details

---
+++ src/components/DetailsContainer.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export function DetailsContainer({ cardMap }) {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row gap-6 mt-10">
+        <div className="flex-1">
+          <img src={cardMap.image} alt="" />
+
+          <div className="flex flex-col md:flex-row gap-6 mt-6">
+            <div className="flex-1">
+              <h2 className="text-2xl font-semibold text-gray-900 mb-1">
+                {cardMap.map_name}
+              </h2>
+              <p className="text-gray-500">{cardMap.description}</p>
+
+              <div className="flex flex-col md:flex-row gap-6 mt-6">
+                <div className="flex-1">
+                  <h1 className="text-2xl font-semibold text-gray-900 mb-1">
+                    Enviado por:
+                  </h1>
+                  <h2 className="text-xl font-semibold text-gray-900 mb-1">
+                    {cardMap.name}
+                  </h2>
+                  <p className="text-gray-500">{cardMap.description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="bg-primary text-white block ml-auto px-2 py-1 rounded-bl-lg rounded-tr-lg">
+        <span className="text-sm font-semibold">
+          <Link href="/">Voltar</Link>
+        </span>
+      </button>
+    </>
+  );
+}
