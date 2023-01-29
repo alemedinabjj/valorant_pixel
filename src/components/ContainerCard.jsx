fb7137059980b885ea3dadb23f95b3604cbf9275
Alexandre Medina <alesurf13@gmail.com>
2023-01-29 11:34:20 -0300

    new feat divider

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,8 +1,10 @@
 import { Card } from "./Card";
+import { NamePage } from "./NamePage";
 
 export function ContainerCard() {
   return (
     <section className="flex-1">
+      <NamePage label="All Pixels" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <Card />
         <Card />
